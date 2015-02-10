f08fe0acbe28330b02d02af78de711eb34d127f0
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-02-10 18:29:08 +0100

    Refactor tests

--- test/linux/test0/run.sh
+++ test/linux/test0/run.sh
@@ -7,8 +7,8 @@ srcdir=`dirname $0`
 srcdir=`cd $srcdir; pwd`
 dstdir=`pwd`
 
-bindir=$srcdir/../..
-npm=$srcdir/../../node_modules/.bin
+bindir=$srcdir/../../..
+npm=$bindir/node_modules/.bin
 
 ($bindir/index.js run -e 'mnode' 'console.log("hi!")') > $srcdir/output
 $npm/diff-files -m "Test safe jail: should allow node printing a message" $srcdir/output $srcdir/reference
--- test/linux/test1/run.sh
+++ test/linux/test1/run.sh
@@ -7,8 +7,8 @@ srcdir=`dirname $0`
 srcdir=`cd $srcdir; pwd`
 dstdir=`pwd`
 
-bindir=$srcdir/../..
-npm=$srcdir/../../node_modules/.bin
+bindir=$srcdir/../../..
+npm=$bindir/node_modules/.bin
 
 ($bindir/index.js run -e 'mnode' 'console.log(require("fs").readdirSync("/"))') | sed -e '/tmp/d' > $srcdir/output
 $npm/diff-files -m "Test safe jail: should not allow node accessing root directory" $srcdir/output $srcdir/reference
